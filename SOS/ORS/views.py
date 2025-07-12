f796b9ea14362e1cfc5c84ab67737d52674a6429
SawanPanwar <skpanwar25@gmail.com>
2025-07-12 18:16:29 +0530

    12-07-2025

---
+++ SOS/ORS/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import redirect, render
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.sessions.models import Session
+
+from .ctl.RegistrationCtl import RegistrationCtl
+from .ctl.LoginCtl import LoginCtl
+from .ctl.WelcomeCtl import WelcomeCtl
+from .ctl.UserListCtl import UserListCtl
+
+
+@csrf_exempt
+def action(request, page="", operation="", id=0):
+    if page == "Logout":
+        Session.objects.all().delete()
+        request.session['user'] = None
+        page = "Login"
+    ctlName = page + "Ctl()"
+    ctlObj = eval(ctlName)
+    res = ctlObj.execute(request, {"operation": operation, "id": id})
+    return res
+
+
+def index(request):
+    res = render(request, 'Welcome.html')
+    return res
